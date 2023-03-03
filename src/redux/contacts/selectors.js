b5e52765f349a8df993f3ce184bcd1e1211a3a8a
KaterynaChernetska <vesnavved@gmail.com>
2023-03-03 11:36:38 +0200

    Add createSelector

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,11 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contactsData.contacts.items;
 export const selectLoader = state => state.contactsData.contacts.isLoading;
 export const selectError = state => state.contactsData.contacts.error;
 export const selectFilter = state => state.contactsData.filter;
 
-
-export const selectFilteredContacts = state => {
-    return state.contactsData.contacts.items.filter(({ name }) =>
-      name.toLowerCase().includes(state.contactsData.filter.toLowerCase())
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
     );
-  };
+  }
+);
